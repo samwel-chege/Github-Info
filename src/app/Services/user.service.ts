16f54c9bc184d020bda42ae2c8fa9a48e8cf93ad
samwel-chege <samwelchegeh09@gmail.com>
2021-07-25 23:51:30 +0300

    add a new service

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { User } from '../Interfaces/user';
+import { Repository } from '../Interfaces/Repo';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+ foundUser: User;
+ allRepos: Repository;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.foundUser = new User("","","","",0,0,0,"",new Date);
+    this.allRepos = new Repository("","","",new Date,0,0,"");
+  }
+  searchUser(searchName: string) {
+   
+    interface Response {
+      url:string,
+      login: string;
+      html_url:string;
+      location:string
+      public_repos:number;
+      followers:number;
+      following:number;
+      avatar_url:string;
+      created_at:Date;
+    }
+
+    return new Promise((resolve:any, reject) => {
+      this.http.get<Response>('https://api.github.com/users/'+searchName+'?access_token='+environment.apiKey).toPromise().then(
+        (result) => {
+          this.foundUser = result;
+          console.log(this.foundUser);
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+  }
+  getRepos(searchName:any){
+    interface Repos{
+      name:string;
+      html_url:string;
+      description:string;
+      forks:number;
+      watchers_count:number;
+      language:string;
+      created_at:Date;
+    }
+    return new Promise((resolve:any,reject)=>{
+      this.http.get<Repos>('https://api.github.com/users/'+searchName+"/repos?order=created&sort=asc?access_token="+environment.apiKey).toPromise().then(
+        (results) => {
+          this.allRepos = results;
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+  }
 }
