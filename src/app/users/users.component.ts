23d84599a59b74f112ad635d5d39d1345028aeec
samwel-chege <samwelchegeh09@gmail.com>
2021-07-26 00:06:44 +0300

    add additiaonal changes

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../Services/user.service';
+import { Repository } from '../Interfaces/Repo';
+import { User } from '../Interfaces/user';
+
+
 
 @Component({
   selector: 'app-users',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user!: User;
+  repo!: Repository;
 
-  constructor() { }
+  constructor(public myService: UserService, private repoService: UserService) { }
+
+  searchs(searchName:any) {
+    this.myService.searchUser(searchName).then(
+      (success)=>{
+        this.user = this.myService.foundUser;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    );
+      this.repoService.getRepos(searchName).then(
+        (results)=>{
+          this.repo =this.repoService.allRepos
+          console.log(this.repo);
+        },
+        (error)=>{
+          console.log(error);
+        }
+      );
+  }
 
   ngOnInit(): void {
+    this.searchs('samwel-chege');
   }
 
 }
