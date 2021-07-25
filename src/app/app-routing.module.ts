668e5f400dc9bbbf74720dbb1491821c5156aacb
samwel-chege <samwelchegeh09@gmail.com>
2021-07-25 23:55:54 +0300

    initiate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { UsersComponent } from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user-profile', component: UsersComponent },
+  {path: 'search-repo', component: ReposComponent},
+  {path: '', redirectTo: '/user-profile', pathMatch: 'full' },
+  {path:"**",component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
+  
 })
 export class AppRoutingModule { }
